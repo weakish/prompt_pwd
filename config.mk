5d6c5169792d9431c077fb2ad4a97120d7258a9a
Jakukyo Friel <weakish@gmail.com>
2016-11-13 20:48:03 +0800

    More warnnings to build option.

--- config.mk
+++ config.mk
@@ -2,5 +2,5 @@
 PREFIX = /usr/local
 
 # cc options
-CFLAGS += -Wall -std=c11 -Ofast
+CFLAGS += -std=c11 -Wall -Wc90-c99-compat -Wc99-c11-compat -Wc++-compat -Wextra -Wtype-limits -Wconversion -Ofast
 CC ?= cc
