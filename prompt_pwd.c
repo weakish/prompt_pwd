8fcaa4387d2e7f077829434a51825cb1bd6708fe
Jakukyo Friel <weakish@gmail.com>
2016-11-13 20:48:03 +0800

    Not depend on `sysexits.h` (unavailable on windows).

--- prompt_pwd.c
+++ prompt_pwd.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sysexits.h>
+
+#define EX_USAGE        64      /* command line usage error */
+#define EX_SOFTWARE     70      /* internal software error */
 
 #define LENGTH_MAX 80
 
